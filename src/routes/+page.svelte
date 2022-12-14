532257f2ff4231bcbbbe098486305e011214510c
mmerle <35971019+mmerle@users.noreply.github.com>
2022-12-14 15:45:39 -0500

    update green + sizing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,23 @@
 <h1>Mallard Green</h1>
 <p>
-  Web development studio of <a href='https://mmerle.dev' target='_blank'>mmerle</a> — <a href='mailto:hello@mmerle.dev'>hello@mmerle.dev</a>
+  Web development studio of
+  <a href="https://mmerle.dev" target="_blank" rel="noreferrer">mmerle</a> —
+  <a href="mailto:hello@mmerle.dev">hello@mmerle.dev</a>
 </p>
 
 <style>
   h1 {
-    font-size: max(8vw, 2rem);
-    line-height: 1.1;
-    letter-spacing: -1px;
-    margin-block: 0;
+    font-size: max(2.5vw, 2.5rem);
+    line-height: 1.125;
+    letter-spacing: -1.5px;
+    margin-bottom: 1rem;
   }
-  
+
   p {
-    font-size: 18px;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: max(0.8vw, 1.125rem);
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-weight: 500;
+    letter-spacing: -0.125px;
   }
 </style>
