c0d0f942a231a21dd68b8d211bfde396c62da2e3
mmerle <35971019+mmerle@users.noreply.github.com>
2024-06-12 01:28:40 -0400

    add biz card

---
+++ src/components/biz-card/index.jsx
@@ -0,0 +1,45 @@
+import cn from 'clsx';
+import s from './biz-card.module.css';
+import useStore from '@libs/store';
+
+export default function BizCard() {
+  const { isBizCardVisible, toggleBizCard } = useStore();
+
+  return (
+    <div className={s.card} data-visible={isBizCardVisible}>
+      <button className={s['card-backdrop']} onClick={toggleBizCard}>
+        Close
+      </button>
+      <div className={s['card-inner']}>
+        <div className={s['card-inner-area']}>
+          <a href="mailto:m@mallard.green">m@mallard.green</a>
+        </div>
+        <div className={s['card-inner-area']}>
+          <a href="https://cal.com/mallardgreen" target="_blank">
+            Book a chat
+          </a>
+        </div>
+        <div className={s['card-inner-area']}>
+          <p>
+            Design Office
+            <br /> An artificial space for real design.
+          </p>
+        </div>
+        <div className={s['card-inner-area']}>
+          <a href="#">Instagram</a>
+        </div>
+      </div>
+      <span className={s['card-close']}>Close x</span>
+    </div>
+  );
+}
+
+export function BizCardToggle({ children, className }) {
+  const { toggleBizCard } = useStore();
+
+  return (
+    <button onClick={toggleBizCard} className={cn(s.toggle, className)}>
+      {children}
+    </button>
+  );
+}
--- src/libs/store.js
+++ src/libs/store.js
@@ -4,6 +4,9 @@ const useStore = create((set) => ({
   isGridOverlayVisible: false,
   toggleGridOverlayVisibility: () =>
     set((state) => ({ isGridOverlayVisible: !state.isGridOverlayVisible })),
+
+  isBizCardVisible: false,
+  toggleBizCard: () => set((state) => ({ isBizCardVisible: !state.isBizCardVisible })),
 }));
 
 export default useStore;
